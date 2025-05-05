32ec50b696acf5b8bc0715fee04cb7488bac5c89
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-05-05 19:13:06 +0200

    Add Jumbotron component with gallery slider and update Home page layout

--- frontend/src/components/jumbotron/jumbotron.jsx
+++ frontend/src/components/jumbotron/jumbotron.jsx
@@ -1,8 +1,75 @@
 import './jumbotron.css';
-export default function Jumbotron() {
+import React, { useState } from "react";
+
+const slidesData = [
+    {
+        id: 1,
+        imgPrev: "https://bato-web-agency.github.io/bato-shared/img/gallery-slider/img1.1.jpg",
+        imgNext: "https://bato-web-agency.github.io/bato-shared/img/gallery-slider/img1.jpg",
+        alt: "Gallery Slide - 1",
+    },
+    {
+        id: 2,
+        imgPrev: "https://bato-web-agency.github.io/bato-shared/img/gallery-slider/img2.1.jpg",
+        imgNext: "https://bato-web-agency.github.io/bato-shared/img/gallery-slider/img2.jpg",
+        alt: "Gallery Slide - 2",
+    },
+    {
+        id: 3,
+        imgPrev: "https://bato-web-agency.github.io/bato-shared/img/gallery-slider/img3.1.jpg",
+        imgNext: "https://bato-web-agency.github.io/bato-shared/img/gallery-slider/img3.jpg",
+        alt: "Gallery Slide - 3",
+    },
+    {
+        id: 4,
+        imgPrev: "https://bato-web-agency.github.io/bato-shared/img/gallery-slider/img4.1.jpg",
+        imgNext: "https://bato-web-agency.github.io/bato-shared/img/gallery-slider/img4.jpg",
+        alt: "Gallery Slide - 4",
+    },
+    {
+        id: 5,
+        imgPrev: "https://bato-web-agency.github.io/bato-shared/img/gallery-slider/img5.1.jpg",
+        imgNext: "https://bato-web-agency.github.io/bato-shared/img/gallery-slider/img5.jpg",
+        alt: "Gallery Slide - 5",
+    },
+];
+
+const Jumbotron = () => {
+    const [activeSlide, setActiveSlide] = useState(1);
+
+    const handleSlideClick = (id) => {
+        setActiveSlide(id);
+    };
     return (
-        <div className="jumbotron">
-            <h1>ciao</h1>
+        <div className="jumbotron" style={{ width: '100%', overflow: 'hidden' }}>
+            <div className="gallery-slider">
+                <div className="gallery-slider__wrapper">
+                    {slidesData.map((slide) => (
+                        <div
+                            key={slide.id}
+                            className={`gallery-slider__slide ${slide.id === activeSlide ? "active" : ""}`}
+                            onClick={() => handleSlideClick(slide.id)}
+                        >
+                            <div className="gallery-slider__image">
+                                <img
+                                    className="gallery-slider__img-prev"
+                                    src={slide.imgPrev}
+                                    alt={slide.alt}
+                                />
+                                <img
+                                    className="gallery-slider__img-next"
+                                    src={slide.imgNext}
+                                    alt={slide.alt}
+                                />
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
-    )
+    );
 }
+
+
+
+export default Jumbotron;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Jumbotron from "../components/jumbotron/jumbotron";
 export default function Home() {
 
     return (
-        <div>
+        <div className="main">
             <h1>Welcome to the Home Page</h1>
             <Jumbotron></Jumbotron>
         </div>
