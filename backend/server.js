309f00c563bc9e20806394e7f8d627b7ff890885
Leonardo-Mocchi <leonardo.mocchi@gmail.com>
2025-05-08 12:24:38 +0200

    Add order management routes and example request bodies to server.js

--- backend/server.js
+++ backend/server.js
@@ -59,4 +59,63 @@ http://localhost:3000/api/v1/products/email
 // 10 random products
 http://localhost:3000/api/v1/products/random
 
+
+// orders routes
+    // CREATE AN ORDER
+        POST http://localhost:3000/api/v1/products/orders
+
+        EXAMPLE BODY:
+        {
+        "customer_info": {
+            "first_name": "Elizabeth",
+            "last_name": "Swann",
+            "email": "elizabeth@blackpearl.com",
+            "phone": "555-123-4567",
+            "address": "23 Pearl Street",
+            "city": "Port Royal",
+            "state": "Caribbean",
+            "postal_code": "12345",
+            "country": "Jamaica"
+        },
+        "items": [
+            {
+            "product_id": 1,
+            "product_variation_id": 4,
+            "quantity": 1,
+            "price": 1200.00
+            },
+            {
+            "product_id": 2,
+            "product_variation_id": null,
+            "quantity": 2,
+            "price": 150.00
+            }
+        ],
+        "delivery": 15.00,
+        "total": 1500.00,
+        "discount": 50.00
+        }
+
+    // DISPLAY ALL DETAILS OF ONE ORDER WITH ORDER ID (admin-side)
+        GET http://localhost:3000/api/v1/products/orders/:id
+
+    // DISPLAY SUMMARY OF THE ORDER BY USING MAIL AND ORDER_ID (client-side)
+        POST http://localhost:3000/api/v1/products/orders/track
+        
+        EXAMPLE BODY:
+        {
+            "email": "elizabeth@blackpearl.com",
+            "order_id": 1
+        }
+    
+    // RETRIEVE ALL ORDERS ASSOCIATED WITH THE SAME EMAIL
+        GET http://localhost:3000/api/v1/products/orders/email/:email
+
+    // UPDATE STATUS OF THE ORDER (admin-side) (Pending, Processing, Completed, Cancelled)
+        PATCH http://localhost:3000/api/v1/products/orders/:id/status
+
+        EXAMPLE BODY:
+        {
+            "status": "Processing"  
+        }
 */
