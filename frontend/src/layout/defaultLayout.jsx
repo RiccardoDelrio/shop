e14317a63567594e266af53bab9f158af3e2fe0a
Leonardo-Mocchi <leonardo.mocchi@gmail.com>
2025-05-14 10:44:19 +0200

    feat: Integrate WelcomePopup component into Home page and adjust localStorage handling in PagePopup

--- frontend/src/layout/defaultLayout.jsx
+++ frontend/src/layout/defaultLayout.jsx
@@ -1,12 +1,10 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar/Navbar'
 import Footer from '../components/footer/footer'
-import PagePopup from '../components/Popup/PagePopup'
 
 function DefaultLayout() {
     return (
         <>
-            <PagePopup />
             <Navbar />
             <div className="main-container">
                 <Outlet />
